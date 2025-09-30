dfa4448813f3320fef61e6d55c78c990e52b850e
francesanne <francesanne.riconalla>
2025-09-30 22:28:50 +0800

    feature(login): add static directory with CSS/JS setup

--- practice/login/views.py
+++ practice/login/views.py
@@ -1,9 +1,8 @@
 # Create your views here.
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import    HttpResponse
 def home(request):
-    return render(request, "login/home.html")
+    return render(request, 'login/index.html')
 
 def login_view(request):
-    return render(request, "login/login.html")
+    return render(request, 'login/login.html')
